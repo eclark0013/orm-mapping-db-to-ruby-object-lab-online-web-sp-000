f7905013c43e890ddfa5fd57a08c083e960c8b77
Eric Clark <eclark0013@gmail.com>
2019-08-10 17:05:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,6 +71,18 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).first.new_from_db
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
